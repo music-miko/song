bc6bb350ac783d80e23a4697f00de810bfff69b8
music-miko <voilet856@gmail.com>
2025-06-13 05:20:25 +0530

    Update db.py

--- DeadlineTech/db.py
+++ DeadlineTech/db.py
@@ -3,10 +3,10 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb+srv://songcounts:cWjZPk0lFdWJ6tXj@music.atn13th.mongodb.net/?retryWrites=true&w=majority&appName=music")
 db = client["music"]
-collection = db["song"]
+song_collection = db["song"]
 
 def is_song_sent(video_id: str) -> bool:
-    return collection.find_one({"video_id": video_id}) is not None
+    return song_collection.find_one({"video_id": video_id}) is not None
 
 # Save song with file_id
 def mark_song_as_sent(video_id: str, file_id: str):
