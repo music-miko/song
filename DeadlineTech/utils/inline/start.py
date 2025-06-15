046ca2a926cc22a432fd98dbc6674b489e8e6a18
music-miko <voilet856@gmail.com>
2025-06-16 04:00:46 +0530

    Update start.py

--- DeadlineTech/utils/inline/start.py
+++ DeadlineTech/utils/inline/start.py
@@ -1,34 +1,45 @@
-from pyrogram.types import InlineKeyboardButton
-from pyrogram.types import WebAppInfo
-
+from pyrogram.types import InlineKeyboardButton, WebAppInfo
 import config
 from DeadlineTech import app
-
 
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startgroup=true"
+            ), 
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
         ],
+        [
+            InlineKeyboardButton(
+                text=_["S_B_5"],
+                url="https://github.com/DeadlineTech/music"
+            )
+        ]
     ]
     return buttons
 
 
 def private_panel(_):
     buttons = [
-        [InlineKeyboardButton(text=_["S_B_6"], web_app=WebAppInfo(url="https://deadlinetech.site"))],
-        [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL),
-        ],
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                url=f"https://t.me/{app.username}?startgroup=true"
             )
         ],
+        [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+        ],
+
+        [
+            InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL)
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], web_app=WebAppInfo(url="https://vps-kappa.vercel.app")), 
+            InlineKeyboardButton(text=_["S_B_5"], url="https://github.com/DeadlineTech/music")
+        ]
     ]
     return buttons
