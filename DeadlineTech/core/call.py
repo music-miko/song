051b69e04bb4b6134363d43488081ffbcb38bab2
music-miko <voilet856@gmail.com>
2025-06-19 03:18:20 +0530

    Update call.py

--- DeadlineTech/core/call.py
+++ DeadlineTech/core/call.py
@@ -590,7 +590,7 @@ class Call(PyTgCalls):
         return str(round(sum(pings) / len(pings), 3))
 
     async def start(self):
-        LOGGER(__name__).info("Starting PyTgCalls Client...\n")
+        LOGGER(__name__).info("Launching PyTgCalls Client...\n")
         if config.STRING1:
             await self.one.start()
         if config.STRING2:
