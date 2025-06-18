29f88518e7fe75307453bd4b352d69b6dfadf48d
music-miko <voilet856@gmail.com>
2025-06-19 04:24:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -104,7 +104,6 @@ SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/1xn73k.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/1xn73k.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/1xn73k.jpg"
 
-PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", False)
 
 def time_to_seconds(time):
     stringt = str(time)
