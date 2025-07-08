9a28fc189588bf74a98eda4e4034e39463e4b73b
music-miko <voilet856@gmail.com>
2025-07-08 17:50:41 +0530

    Update dev.py

--- DeadlineTech/plugins/tools/dev.py
+++ DeadlineTech/plugins/tools/dev.py
@@ -14,16 +14,12 @@ from DeadlineTech import app
 from config import OWNER_ID
 
 
-# Shared global scope for evaluation
-global_vars = globals().copy()
-
 async def aexec(code, client, message):
     exec(
-        "async def __aexec(client, message):\n"
-        + "\n".join(f"    {line}" for line in code.split("\n")),
-        global_vars,
+        "async def __aexec(client, message): "
+        + "".join(f"\n {a}" for a in code.split("\n"))
     )
-    return await global_vars["__aexec"](client, message)
+    return await locals()["__aexec"](client, message)
 
 
 async def edit_or_reply(msg: Message, **kwargs):
@@ -32,12 +28,25 @@ async def edit_or_reply(msg: Message, **kwargs):
     await func(**{k: v for k, v in kwargs.items() if k in spec})
 
 
-@app.on_edited_message(filters.command("eval") & filters.user(OWNER_ID))
-@app.on_message(filters.command("eval") & filters.user(OWNER_ID))
+@app.on_edited_message(
+    filters.command("eval")
+    & filters.user(OWNER_ID)
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
+@app.on_message(
+    filters.command("eval")
+    & filters.user(OWNER_ID)
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
 async def executor(client: app, message: Message):
     if len(message.command) < 2:
         return await edit_or_reply(message, text="<b>ᴡʜᴀᴛ ʏᴏᴜ ᴡᴀɴɴᴀ ᴇxᴇᴄᴜᴛᴇ ʙᴀʙʏ ?</b>")
-    cmd = message.text.split(" ", maxsplit=1)[1]
+    try:
+        cmd = message.text.split(" ", maxsplit=1)[1]
+    except IndexError:
+        return await message.delete()
     t1 = time()
     old_stderr = sys.stderr
     old_stdout = sys.stdout
@@ -68,11 +77,18 @@ async def executor(client: app, message: Message):
             out_file.write(str(evaluation))
         t2 = time()
         keyboard = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("⏳", callback_data=f"runtime {t2-t1:.3f} Seconds")]]
+            [
+                [
+                    InlineKeyboardButton(
+                        text="⏳",
+                        callback_data=f"runtime {t2-t1} Seconds",
+                    )
+                ]
+            ]
         )
         await message.reply_document(
             document=filename,
-            caption=f"<b>⥤ ᴇᴠᴀʟ :</b>\n<code>{cmd[:980]}</code>\n\n<b>⥤ ʀᴇsᴜʟᴛ :</b>\nAttached Document",
+            caption=f"<b>⥤ ᴇᴠᴀʟ :</b>\n<code>{cmd[0:980]}</code>\n\n<b>⥤ ʀᴇsᴜʟᴛ :</b>\nAttached Document",
             quote=False,
             reply_markup=keyboard,
         )
@@ -81,10 +97,18 @@ async def executor(client: app, message: Message):
     else:
         t2 = time()
         keyboard = InlineKeyboardMarkup(
-            [[
-                InlineKeyboardButton("⏳", callback_data=f"runtime {round(t2 - t1, 3)} Seconds"),
-                InlineKeyboardButton("🗑", callback_data=f"forceclose abc|{message.from_user.id}")
-            ]]
+            [
+                [
+                    InlineKeyboardButton(
+                        text="⏳",
+                        callback_data=f"runtime {round(t2-t1, 3)} Seconds",
+                    ),
+                    InlineKeyboardButton(
+                        text="🗑",
+                        callback_data=f"forceclose abc|{message.from_user.id}",
+                    ),
+                ]
+            ]
         )
         await edit_or_reply(message, text=final_output, reply_markup=keyboard)
 
@@ -98,43 +122,90 @@ async def runtime_func_cq(_, cq):
 @app.on_callback_query(filters.regex("forceclose"))
 async def forceclose_command(_, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
-    query, user_id = callback_data.split("|")
+    callback_request = callback_data.split(None, 1)[1]
+    query, user_id = callback_request.split("|")
     if CallbackQuery.from_user.id != int(user_id):
-        return await CallbackQuery.answer(
-            "» ɪᴛ'ʟʟ ʙᴇ ʙᴇᴛᴛᴇʀ ɪғ ʏᴏᴜ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs ʙᴀʙʏ.", show_alert=True
-        )
+        try:
+            return await CallbackQuery.answer(
+                "» ɪᴛ'ʟʟ ʙᴇ ʙᴇᴛᴛᴇʀ ɪғ ʏᴏᴜ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs ʙᴀʙʏ.", show_alert=True
+            )
+        except:
+            return
     await CallbackQuery.message.delete()
-    await CallbackQuery.answer()
+    try:
+        await CallbackQuery.answer()
+    except:
+        return
 
 
-@app.on_edited_message(filters.command("sh") & filters.user(OWNER_ID))
-@app.on_message(filters.command("sh") & filters.user(OWNER_ID))
+@app.on_edited_message(
+    filters.command("sh")
+    & filters.user(OWNER_ID)
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
+@app.on_message(
+    filters.command("sh")
+    & filters.user(OWNER_ID)
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
 async def shellrunner(_, message: Message):
     if len(message.command) < 2:
         return await edit_or_reply(message, text="<b>ᴇxᴀᴍᴩʟᴇ :</b>\n/sh git pull")
     text = message.text.split(None, 1)[1]
-    output = ""
-    try:
-        shell = re.split(r""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", text)
-        process = subprocess.Popen(shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        output = stdout.decode().strip() or stderr.decode().strip()
-    except Exception as err:
-        error_trace = traceback.format_exc()
-        return await edit_or_reply(message, text=f"<b>ERROR :</b>\n<pre>{error_trace}</pre>")
-
-    if not output:
-        output = "None"
-    if len(output) > 4096:
-        with open("output.txt", "w+") as file:
-            file.write(output)
-        await app.send_document(
-            message.chat.id,
-            "output.txt",
-            reply_to_message_id=message.id,
-            caption="<code>Output</code>",
-        )
-        os.remove("output.txt")
+    if "\n" in text:
+        code = text.split("\n")
+        output = ""
+        for x in code:
+            shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", x)
+            try:
+                process = subprocess.Popen(
+                    shell,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
+            except Exception as err:
+                await edit_or_reply(message, text=f"<b>ERROR :</b>\n<pre>{err}</pre>")
+            output += f"<b>{code}</b>\n"
+            output += process.stdout.read()[:-1].decode("utf-8")
+            output += "\n"
     else:
+        shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", text)
+        for a in range(len(shell)):
+            shell[a] = shell[a].replace('"', "")
+        try:
+            process = subprocess.Popen(
+                shell,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+        except Exception as err:
+            print(err)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type,
+                value=exc_obj,
+                tb=exc_tb,
+            )
+            return await edit_or_reply(
+                message, text=f"<b>ERROR :</b>\n<pre>{''.join(errors)}</pre>"
+            )
+        output = process.stdout.read()[:-1].decode("utf-8")
+    if str(output) == "\n":
+        output = None
+    if output:
+        if len(output) > 4096:
+            with open("output.txt", "w+") as file:
+                file.write(output)
+            await app.send_document(
+                message.chat.id,
+                "output.txt",
+                reply_to_message_id=message.id,
+                caption="<code>Output</code>",
+            )
+            return os.remove("output.txt")
         await edit_or_reply(message, text=f"<b>OUTPUT :</b>\n<pre>{output}</pre>")
+    else:
+        await edit_or_reply(message, text="<b>OUTPUT :</b>\n<code>None</code>")
     await message.stop_propagation()
