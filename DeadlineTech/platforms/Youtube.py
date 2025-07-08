c134fd22999cbff24f4f85a22e1055660bdcf3c6
Raghav-afk25 <raghavagarwalq12345@gmail.com>
2025-07-09 03:31:46 +0530

    Update Youtube.py

--- DeadlineTech/platforms/Youtube.py
+++ DeadlineTech/platforms/Youtube.py
@@ -67,7 +67,7 @@ def extract_video_id(link: str) -> str:
 
 
 def api_dl(video_id: str) -> str | None:
-    api_url = f"{API_BASE_URL}/download/song/{video_id}?key={API_KEY}"
+    api_url = f"{API_BASE_URL}/download/song/{video_id}"
     file_path = os.path.join("downloads", f"{video_id}.mp3")
 
     # ✅ Check if already downloaded
