2110d9acf34c762d7575eb28213a15c54dce8726
music-miko <voilet856@gmail.com>
2025-07-27 08:22:52 +0530

    Update start.py

--- DeadlineTech/utils/inline/start.py
+++ DeadlineTech/utils/inline/start.py
@@ -35,7 +35,7 @@ def private_panel(_):
         ],
 
         [
-            InlineKeyboardButton(text=_["S_B_10"], url=f"tg://user?id=7765692814"),
+            InlineKeyboardButton(text=_["S_B_10"], user_id=7321657753"),
             InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT)
             
         ],
