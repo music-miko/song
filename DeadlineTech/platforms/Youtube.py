7c17cf3cf36be032f2b5255027d14ef16f560466
Raghav-afk25 <raghavagarwalq12345@gmail.com>
2025-08-09 13:34:30 +0530

    Update Youtube.py

--- DeadlineTech/platforms/Youtube.py
+++ DeadlineTech/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from DeadlineTech.utils.database import is_on_off
+from DeadlineTech.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
@@ -344,3 +344,4 @@ class YouTubeAPI:
             direct = True
             downloaded_file = await loop.run_in_executor(None, audio_dl)
         return downloaded_file, direct
+
