dec1c8fdb166e88816954c8ec074a50f7430f779
Raghav-afk25 <raghavagarwalq12345@gmail.com>
2025-08-20 20:34:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 #Get API_KEY from @DeadlineTechOwner or @DeadlineApiBot
-API_URL = getenv("API_URL", "http://64.227.79.220:5055")
+API_URL = getenv("API_URL", "https://filegobot.onrender.com")
 API_KEY = getenv("API_KEY")
 
 # Get your mongo url from cloud.mongodb.com
